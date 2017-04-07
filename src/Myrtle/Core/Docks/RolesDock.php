9b1ba0fc67f7dccc726e74ca7e1592e3fc954674
Justin Seliga <jseliga@agilesdesign.com>
2017-04-07 07:32:12 -0400

    fix path for config files

--- src/Myrtle/Core/Docks/RolesDock.php
+++ src/Myrtle/Core/Docks/RolesDock.php
@@ -42,8 +42,8 @@ class RolesDock extends Dock
     public function configPaths()
     {
         return [
-            'docks.' . self::class => dirname(__DIR__, 2) . '/config/docks/roles.php',
-            'abilities' => dirname(__DIR__, 2) . '/config/abilities.php',
+            'docks.' . self::class => dirname(__DIR__, 3) . '/config/docks/roles.php',
+            'abilities' => dirname(__DIR__, 3) . '/config/abilities.php',
         ];
     }
 
@@ -55,7 +55,7 @@ class RolesDock extends Dock
     public function migrationPaths()
     {
         return [
-            dirname(__DIR__, 2) . '/database/migrations',
+            dirname(__DIR__, 3) . '/database/migrations',
         ];
     }
 
@@ -67,7 +67,7 @@ class RolesDock extends Dock
     public function routes()
     {
         return [
-            'admin' => dirname(__DIR__, 2) . '/routes/admin.php',
+            'admin' => dirname(__DIR__, 3) . '/routes/admin.php',
         ];
     }
 }
